487ba7a53d164b9d76baf28545d6a4d07cd45bb8
subtanjiroo <138212507+subtanjiroo@users.noreply.github.com>
2024-11-12 17:00:22 +0700

    Delete tomishow directory

--- tomishow/static/src/js/History_API.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module **/
-
-export function HistoryBar() {
-    console.log("HistoryBar triggered.");
-    // Thêm logic GET HISTORY API nếu cần
-}
--- tomishow/static/src/js/Upload_API.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module **/
-
-export async function UploadHandle() {
-    console.log("UploadHandle triggered.");
-    // Thêm logic upload file tại đây
-}
